b2cbb8fcdcc2f4ba71f9c60f24d7eeb6bb418c6c
Alexander Korzhov <alexander.korzhov@gmail.com>
2025-06-15 00:57:23 +0300

    add truncate tables after e2e tests

--- backend/test/e2e/auth.e2e.spec.ts
+++ backend/test/e2e/auth.e2e.spec.ts
@@ -3,17 +3,29 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../../src/app.module';
+import { PrismaService } from '../../src/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let prismaService: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prismaService = moduleFixture.get(PrismaService);
     await app.init();
+  });
+
+  beforeEach(async () => {
+    await prismaService.truncateAllTables();
+  });
+
+  afterAll(async () => {
+    await prismaService.truncateAllTables();
+    await prismaService.$disconnect();
   });
 
   describe('auth', () => {
